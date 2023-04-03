5604010ce50dc1b4d5cab8cd1758ab526f4192b1
Mohammed Slimane <mohammed.slimane@museedelhistoire.ca>
2023-04-03 07:06:31 -0400

    optimize and refactor index code

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,24 @@
+//
 const { DateTime } = require("luxon");
 
 //
+const date_filter_01 = dateObj => 
+    DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+
+/*
+const date_filter_01 = (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED); }
+*/
+//
 module.exports = function (eleventyConfig){
+    /*
     eleventyConfig.addPassthroughCopy('./src/assets/css/style.css')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+    */
     eleventyConfig.addPassthroughCopy('./src/assets')
 
     //
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
+    eleventyConfig.addFilter("postDate", date_filter_01);
 
     //
     return {
